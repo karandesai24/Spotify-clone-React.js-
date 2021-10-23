0a0fccd186d0970b3c4aa2db6c8e5f46cd1ee98b
karandesai24 <78134745+karandesai24@users.noreply.github.com>
2021-10-23 23:10:13 +0530

    Spotify-clone

---
+++ src/spotify.js
@@ -0,0 +1,26 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const clientId = "4f0c2a8e7b924f47b02f8718ddb0fc8b";
+const redirectUri = "https://spotify-clone-3f120.web.app";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
